a58e62ff8f22b7c7c018ddea209753d11eb1b302
Neetesh Kumar <165168900+8077364606@users.noreply.github.com>
2025-04-16 11:58:25 +0530

    completed all modules

---
+++ src/constant/__init__.py
@@ -0,0 +1,14 @@
+import os
+
+
+AWS_S3_BUCKET_NAME = "Wafer-fault"
+MONGO_DATABASE_NAME = "PWskills"
+MONGO_COLLECTION_NAME = "wafefault"
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL = "mongodb+srv://Neetesh:12345@cluster0.oibqarz.mongodb.net/?retryWrites=true&w=majority"
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
+
+artifact_folder = "artifacts"
